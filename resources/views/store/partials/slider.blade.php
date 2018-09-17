0ffa17c72bccb1576bdf2f811b369d065b4ac6a0
Kasen Li <kasenkelvin.10@gmail.com>
2018-09-17 01:04:36 -0400

    initial slider (uncompleted)

---
+++ resources/views/store/partials/slider.blade.php
@@ -0,0 +1,26 @@
+<div id="slider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#slider" data-slide-to="0" class="active"></li>
+    <li data-target="#slider" data-slide-to="1"></li>
+    <li data-target="#slider" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://shop.wwe.com/on/demandware.static/-/Sites/default/dw56056ec2/images/slot/landing/top-action-images/Sasha-Banks_Custom-Tees-Banner.png" class="d-block w-100 img-fluid" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img src="https://productimages.withfloats.com/actual/5a438629da8e9f0bb0fec1a6.jpg" class="d-block w-100 img-fluid" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i1.wp.com/ae01.alicdn.com/kf/HTB1UL2.LXXXXXb9XFXXq6xXFXXX3/Ni%C3%B1os-Camisetas-ropa-Ni%C3%B1os-camiseta-manga-larga-estilo-hip-hop-Jers%C3%A9is-ni%C3%B1os-Ni%C3%B1os-blusa-pullover.jpg?crop=5,2,900,500&quality=2880" class="d-block w-100 img-fluid" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
