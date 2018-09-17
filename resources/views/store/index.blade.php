3bf638ba410c7426d0df6996a201c6f45fd3e466
Kasen Li <kasenkelvin.10@gmail.com>
2018-09-17 19:53:26 -0400

    slider ready

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -1,8 +1,6 @@
-@extends('store.template')
+@extends('store.main')
 
 @section('content')
-
-@include('store.partials.slider')
 
 <div class="container text-center">
 	<div id="products">
@@ -14,7 +12,7 @@
 					<p>{{ $product->extract }}</p>
 					<h3><span class="badge badge-success">Precio: ${{number_format($product->price,2)}}</span></h3>
 					<p>
-						<a class="btn btn-warning" href="#"><i class="fa fa-cart-plus"></i> La quiero</a>
+						<a class="btn btn-warning" href="{{ route('cart-add', $product->slug)}}"><i class="fa fa-cart-plus"></i> La quiero</a>
 						<a class="btn btn-info" href="{{ route('product-detail', $product->slug)}}"><i class="fa fa-chevron-circle-right"></i> Leer mas</a>
 					</p>
 				</div>
--- resources/views/store/partials/slider.blade.php
+++ resources/views/store/partials/slider.blade.php
@@ -6,13 +6,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://shop.wwe.com/on/demandware.static/-/Sites/default/dw56056ec2/images/slot/landing/top-action-images/Sasha-Banks_Custom-Tees-Banner.png" class="d-block w-100 img-fluid" src="..." alt="First slide">
+      <img src="{{ asset('images/banners/awesome.jpeg') }}" style="height: 660px;" class="d-block w-100 img-fluid" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img src="https://productimages.withfloats.com/actual/5a438629da8e9f0bb0fec1a6.jpg" class="d-block w-100 img-fluid" src="..." alt="Second slide">
+      <img src="{{ asset('images/banners/tshirt_closelinelg.jpg') }}" style="height: 660px;" class="d-block w-100 img-fluid" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img src="https://i1.wp.com/ae01.alicdn.com/kf/HTB1UL2.LXXXXXb9XFXXq6xXFXXX3/Ni%C3%B1os-Camisetas-ropa-Ni%C3%B1os-camiseta-manga-larga-estilo-hip-hop-Jers%C3%A9is-ni%C3%B1os-Ni%C3%B1os-blusa-pullover.jpg?crop=5,2,900,500&quality=2880" class="d-block w-100 img-fluid" src="..." alt="Third slide">
+      <img src="{{ asset('images/banners/banner.jpg') }}" style="height: 660px;" class="d-block w-100 img-fluid" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
@@ -23,4 +23,4 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>
+</div><hr>
