7985a3f7fc0a774f79edc135b64a04ec09f75d9a
Kasen Li <kasenkelvin.10@gmail.com>
2018-09-17 00:46:48 -0400

    navbar and footer ready

--- resources/views/store/partials/footer.blade.php
+++ resources/views/store/partials/footer.blade.php
@@ -1,1 +1,32 @@
-footer...
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<h3>My Laravel Store</h3>
+			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+		</div>
+		<div class="col-md-4">
+			<h3>Desarrollado por:</h3>
+			<div class="author-info">
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNLzZszQbQf6jkknIGI8A3rj-0BoEngyi9156njfrCjPED9_b2vw" class="avatar">
+				<p><a href="#" style="color: white">@KasenLi</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+			</div>
+			
+		</div>
+		<div class="col-md-4">
+			<h3>Síguenos</h3>
+			<ul class="redes">
+				<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube-square fa-2x"></i></a></li>
+			</ul>
+			<h3>Escríbenos:</h3>
+			<i class="fa fa-at"><a href="#" style="color:white;">kasenkelvin.10@gmail.com</a></i>
+		</div>
+	</div>
+</footer>
--- resources/views/store/partials/nav.blade.php
+++ resources/views/store/partials/nav.blade.php
@@ -1,23 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand main-title" href="#">Tienda</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <span class="navbar-text">My Laravel Store</span>
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+      
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i></a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Conócenos</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Contactos</a></li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user"></i><span class="caret"></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Iniciar Sesión</a>
+        </div>
       </li>
     </ul>
+    
   </div>
 </nav>
