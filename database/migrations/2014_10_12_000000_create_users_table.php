88a54d9366b312cffb68ddb731de400e737fb682
Kasen Li <kasenkelvin.10@gmail.com>
2018-09-21 00:46:39 -0400

    users fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',120);
+            $table->string('name',100);
+            $table->string('last_name',100);
             $table->string('email',155)->unique();
+            $table->string('user', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',128);
+            $table->string('password',60);
+            $table->enum('type', ['user','admin']);
+            $table->boolean('active');
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = array(
+        	[
+        		'name'		=>	'Kasen',
+        		'last_name'	=>	'Li',
+        		'email'		=>	'kasenkelvin.10@gmail.com',
+        		'user'		=>	'kelvin',
+        		'password'	=>	\Hash::make('123456'),
+        		'type'		=>	'admin',
+        		'active'	=>	1,
+        		'address'	=>	'Calle 13',
+        		'created_at'=>	new DateTime,
+        		'updated_at'=>	new DateTime
+        	],
+        	[
+        		'name'		=>	'Jennie',
+        		'last_name'	=>	'Kim',
+        		'email'		=>	'jk@example.com',
+        		'user'		=>	'jennie',
+        		'password'	=>	\Hash::make('123456'),
+        		'type'		=>	'user',
+        		'active'	=>	1,
+        		'address'	=>	'Seul',
+        		'created_at'=>	new DateTime,
+        		'updated_at'=>	new DateTime
+        	]
+        );
+
+        User::insert($data);
+    }
+}
